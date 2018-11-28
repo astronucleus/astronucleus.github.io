4963c64762c741572f3d7614a19ed5a5b6658e9c
migueldeguzman <40692613+migueldeguzman@users.noreply.github.com>
2018-11-28 11:15:35 +0800

    adding files

---
+++ precache-manifest.bb418a41035ce15e46d55862705f3fbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2689ae14bc6ad9c2585bc13224c7f636",
+    "url": "/static/media/logo.2689ae14.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "16eb1ee72c5b60a15f87",
+    "url": "/static/js/main.16eb1ee7.chunk.js"
+  },
+  {
+    "revision": "d01bc2dad32696871ca1",
+    "url": "/static/js/1.d01bc2da.chunk.js"
+  },
+  {
+    "revision": "16eb1ee72c5b60a15f87",
+    "url": "/static/css/main.29900a68.chunk.css"
+  },
+  {
+    "revision": "d01bc2dad32696871ca1",
+    "url": "/static/css/1.ecddc052.chunk.css"
+  },
+  {
+    "revision": "54efab701e2ceb779d1e3ccc27f4d941",
+    "url": "/index.html"
+  }
+];
